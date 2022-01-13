25c6f42f700c7833023a512b74ef37b8a5954c5d
Brad Peng <bradpeng778@gmail.com>
2022-01-13 11:53:31 -0800

    added home page and react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+import Spacepewpew from './components/spacepewpew';
+import Home from './components/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          First commit!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      <Route path="" element={<Home />} />
+      <Route path="/spacepewpew" element={<Spacepewpew />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/home.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import { Typography } from '@mui/material';
+
+import Grid from '@mui/material/Grid';
+import Item from '@mui/material/Grid';
+
+import Timeline from '@mui/lab/Timeline';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+function Home() {
+
+    return (
+
+
+        <div className="App">
+            <div className='aboutMe'>
+                <Grid container spacing={50}>
+                    <Grid item xs={16}>
+                        <Item>
+                            <Typography variant='h3'>Brad Peng</Typography>
+                        </Item>
+                        <Item>
+                            <Typography variant='h5'>Computer Science, The University of British Columbia (2019 - 2024)</Typography>
+                        </Item>
+                    </Grid>
+                    <Grid item xs={16}>
+                        <Item>
+                            <Typography variant='h3'>Work History</Typography>
+                        </Item>
+                        <Item>
+                            <Timeline position="alternate">
+                                <TimelineItem>
+                                    <TimelineOppositeContent >
+                                        <Typography variant='h6'>
+                                            Sep 2021 - Apr 2022
+                                        </Typography>
+
+                                    </TimelineOppositeContent>
+                                    <TimelineSeparator>
+                                        <TimelineDot color="primary" />
+                                        <TimelineConnector />
+                                    </TimelineSeparator>
+                                    <TimelineContent>
+                                        <Card sx={{ maxWidth: 345 }}>
+
+                                            <CardContent>
+                                                <Typography variant="h6">
+                                                    Web Developer (Co-op)
+                                                </Typography>
+                                                <Typography gutterBottom variant='h8'>Bank of Canada</Typography>
+                                                <Typography variant="body2">
+                                                    Worked on maintaining and updating the 5 internal and external sites hosted by the Bank of Canada. Introduced quality of life changes to the PHP component library to allow
+                                                </Typography>
+                                            </CardContent>
+
+                                        </Card>
+                                    </TimelineContent>
+                                </TimelineItem>
+                                <TimelineItem>
+                                    <TimelineOppositeContent>
+                                        <Card sx={{ maxWidth: 345 }}>
+
+                                            <CardContent>
+                                                <Typography variant="h6">
+                                                    Web Developer (Summer Intern)
+                                                </Typography>
+                                                <Typography gutterBottom variant='h8'>Institute of Applied Design and Technology Education Society</Typography>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Worked on maintaining and updating the 5 internal and external sites hosted by the Bank of Canada. Introduced quality of life changes to the PHP component library to allow
+                                                </Typography>
+                                            </CardContent>
+
+                                        </Card>
+                                    </TimelineOppositeContent>
+                                    <TimelineSeparator>
+                                        <TimelineDot color="secondary" />
+                                        <TimelineConnector />
+                                    </TimelineSeparator>
+                                    <TimelineContent>
+                                        <Typography variant='h6'>
+                                            Jul 2020 - Aug 2020
+                                        </Typography>
+                                    </TimelineContent>
+                                </TimelineItem>
+                                <TimelineItem>
+                                    <TimelineOppositeContent >
+                                        <Typography variant='h6'>
+                                            Jul 2018 - Aug 2018
+                                        </Typography>
+                                    </TimelineOppositeContent>
+                                    <TimelineSeparator>
+                                        <TimelineDot color="success" />
+
+                                    </TimelineSeparator>
+                                    <TimelineContent><Card sx={{ maxWidth: 345 }}>
+
+                                        <CardContent>
+                                            <Typography variant="h6">
+                                                iOS Developer (Summer Intern)
+                                            </Typography>
+                                            <Typography gutterBottom variant='h8'>Institute of Applied Design and Technology Education Society</Typography>
+                                            <Typography variant="body2" >
+                                                Worked on maintaining and updating the 5 internal and external sites hosted by the Bank of Canada. Introduced quality of life changes to the PHP component library to allow
+                                            </Typography>
+                                        </CardContent>
+
+                                    </Card>
+                                    </TimelineContent>
+                                </TimelineItem>
+
+                            </Timeline>
+                        </Item>
+
+                        <Item>
+                            <Typography variant='h3'>Projects</Typography>
+                        </Item>
+                    </Grid>
+                </Grid>
+
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Home;
+
