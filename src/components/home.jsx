09162f2c0b916105bc2fadd313c5cb3b5e7e82d6
Brad Peng <bradpeng778@gmail.com>
2022-01-13 18:08:54 -0800

    refactored grid system and added personal projects

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 
 import Grid from '@mui/material/Grid';
 import Item from '@mui/material/Grid';
@@ -15,119 +15,230 @@ import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 
+import { styled } from '@mui/material/styles';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import { red } from '@mui/material/colors';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { maxWidth } from "@mui/system";
+
 function Home() {
-
     return (
-
-
         <div className="App">
-            <div className='aboutMe'>
-                <Grid container spacing={50}>
-                    <Grid item xs={16}>
-                        <Item>
-                            <Typography variant='h3'>Brad Peng</Typography>
-                        </Item>
-                        <Item>
-                            <Typography variant='h5'>Computer Science, The University of British Columbia (2019 - 2024)</Typography>
-                        </Item>
+            <Container>
+
+                <Typography variant='h3' component='h1'>Brad Peng</Typography>
+                <Typography variant='h5'>Computer Science, The University of British Columbia (2019 - 2024)</Typography>
+
+
+
+                <Typography variant='h3' component='h1'>Work History</Typography>
+                <Timeline position="alternate">
+                    <TimelineItem>
+                        <TimelineOppositeContent >
+                            <Typography variant='h6'>
+                                September 2021 - April 2022
+                            </Typography>
+
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineDot color="primary" />
+                            <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent>
+                            <Card sx={{ maxWidth: 500 }}>
+
+                                <CardContent>
+                                    <Typography variant="h6">
+                                        Web Developer (Co-op)
+                                    </Typography>
+                                    <Typography gutterBottom variant='h8'>Bank of Canada</Typography>
+                                    <Typography variant="body2">
+                                        Worked on maintaining and updating the 5 internal and external sites hosted by the Bank of Canada. Introduced quality of life changes to the PHP component library to allow
+                                    </Typography>
+                                </CardContent>
+
+                            </Card>
+                        </TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <TimelineOppositeContent>
+                            <Card sx={{ maxWidth: 500 }}>
+
+                                <CardContent>
+                                    <Typography variant="h6">
+                                        Web Developer (Summer Intern)
+                                    </Typography>
+                                    <Typography gutterBottom variant='h8'>Institute of Applied Design and Technology Education Society</Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Worked on maintaining and updating the 5 internal and external sites hosted by the Bank of Canada. Introduced quality of life changes to the PHP component library to allow
+                                    </Typography>
+                                </CardContent>
+
+                            </Card>
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineDot color="secondary" />
+                            <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent>
+                            <Typography variant='h6'>
+                                July 2020 - August 2020
+                            </Typography>
+                        </TimelineContent>
+                    </TimelineItem>
+                    <TimelineItem>
+                        <TimelineOppositeContent >
+                            <Typography variant='h6'>
+                                July 2018 - August 2018
+                            </Typography>
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineDot color="success" />
+
+                        </TimelineSeparator>
+                        <TimelineContent>
+                            <Card sx={{ maxWidth: 500 }}>
+
+                                <CardContent>
+                                    <Typography variant="h6">
+                                        iOS Developer (Summer Intern)
+                                    </Typography>
+                                    <Typography gutterBottom variant='h8'>Institute of Applied Design and Technology Education Society</Typography>
+                                    <Typography variant="body2" >
+                                        Worked on maintaining and updating the 5 internal and external sites hosted by the Bank of Canada. Introduced quality of life changes to the PHP component library to allow
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </TimelineContent>
+                    </TimelineItem>
+                </Timeline>
+
+                <Typography gutterBottom variant='h3' component='h1'>Personal Projects</Typography>
+
+                <Grid
+                    justifyContent="flex-start"
+                    alignItems="center"
+                    container spacing={12}
+                    rowSpacing={6}>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <Card sx={{ maxWidth: 345, minWidth: 300 }}>
+                            <CardHeader
+
+
+                                title="Space Pew Pew"
+                                subheader="Summer 2021"
+                            />
+                            <CardMedia
+                                component="img"
+                                height="194"
+                                image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                                alt="Paella dish"
+                            />
+                            <CardContent>
+                                <Typography variant="body2" color="text.secondary">
+                                    Shoot my friend bruce in a fun arcade-style 2D top-down shooting game!
+                                </Typography>
+                            </CardContent>
+
+                        </Card>
                     </Grid>
-                    <Grid item xs={16}>
-                        <Item>
-                            <Typography variant='h3'>Work History</Typography>
-                        </Item>
-                        <Item>
-                            <Timeline position="alternate">
-                                <TimelineItem>
-                                    <TimelineOppositeContent >
-                                        <Typography variant='h6'>
-                                            September 2021 - April 2022
-                                        </Typography>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <Card sx={{ maxWidth: 345, minWidth: 300 }}>
+                            <CardHeader
 
-                                    </TimelineOppositeContent>
-                                    <TimelineSeparator>
-                                        <TimelineDot color="primary" />
-                                        <TimelineConnector />
-                                    </TimelineSeparator>
-                                    <TimelineContent>
-                                        <Card sx={{ maxWidth: 345 }}>
 
-                                            <CardContent>
-                                                <Typography variant="h6">
-                                                    Web Developer (Co-op)
-                                                </Typography>
-                                                <Typography gutterBottom variant='h8'>Bank of Canada</Typography>
-                                                <Typography variant="body2">
-                                                    Worked on maintaining and updating the 5 internal and external sites hosted by the Bank of Canada. Introduced quality of life changes to the PHP component library to allow
-                                                </Typography>
-                                            </CardContent>
+                                title="Space Pew Pew"
+                                subheader="Summer 2021"
+                            />
+                            <CardMedia
+                                component="img"
+                                height="194"
+                                image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                                alt="Paella dish"
+                            />
+                            <CardContent>
+                                <Typography variant="body2" color="text.secondary">
+                                    Shoot my friend bruce in a fun arcade-style 2D top-down shooting game!
+                                </Typography>
+                            </CardContent>
 
-                                        </Card>
-                                    </TimelineContent>
-                                </TimelineItem>
-                                <TimelineItem>
-                                    <TimelineOppositeContent>
-                                        <Card sx={{ maxWidth: 345 }}>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <Card sx={{ maxWidth: 345, minWidth: 300 }}>
+                            <CardHeader
 
-                                            <CardContent>
-                                                <Typography variant="h6">
-                                                    Web Developer (Summer Intern)
-                                                </Typography>
-                                                <Typography gutterBottom variant='h8'>Institute of Applied Design and Technology Education Society</Typography>
-                                                <Typography variant="body2" color="text.secondary">
-                                                    Worked on maintaining and updating the 5 internal and external sites hosted by the Bank of Canada. Introduced quality of life changes to the PHP component library to allow
-                                                </Typography>
-                                            </CardContent>
 
-                                        </Card>
-                                    </TimelineOppositeContent>
-                                    <TimelineSeparator>
-                                        <TimelineDot color="secondary" />
-                                        <TimelineConnector />
-                                    </TimelineSeparator>
-                                    <TimelineContent>
-                                        <Typography variant='h6'>
-                                            July 2020 - August 2020
-                                        </Typography>
-                                    </TimelineContent>
-                                </TimelineItem>
-                                <TimelineItem>
-                                    <TimelineOppositeContent >
-                                        <Typography variant='h6'>
-                                            July 2018 - August 2018
-                                        </Typography>
-                                    </TimelineOppositeContent>
-                                    <TimelineSeparator>
-                                        <TimelineDot color="success" />
+                                title="Space Pew Pew"
+                                subheader="Summer 2021"
+                            />
+                            <CardMedia
+                                component="img"
+                                height="194"
+                                image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                                alt="Paella dish"
+                            />
+                            <CardContent>
+                                <Typography variant="body2" color="text.secondary">
+                                    Shoot my friend bruce in a fun arcade-style 2D top-down shooting game!
+                                </Typography>
+                            </CardContent>
 
-                                    </TimelineSeparator>
-                                    <TimelineContent><Card sx={{ maxWidth: 345 }}>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <Card sx={{ maxWidth: 345, minWidth: 300 }}>
+                            <CardHeader
 
-                                        <CardContent>
-                                            <Typography variant="h6">
-                                                iOS Developer (Summer Intern)
-                                            </Typography>
-                                            <Typography gutterBottom variant='h8'>Institute of Applied Design and Technology Education Society</Typography>
-                                            <Typography variant="body2" >
-                                                Worked on maintaining and updating the 5 internal and external sites hosted by the Bank of Canada. Introduced quality of life changes to the PHP component library to allow
-                                            </Typography>
-                                        </CardContent>
 
-                                    </Card>
-                                    </TimelineContent>
-                                </TimelineItem>
+                                title="Space Pew Pew"
+                                subheader="Summer 2021"
+                            />
+                            <CardMedia
+                                component="img"
+                                height="194"
+                                image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                                alt="Paella dish"
+                            />
+                            <CardContent>
+                                <Typography variant="body2" color="text.secondary">
+                                    Shoot my friend bruce in a fun arcade-style 2D top-down shooting game!
+                                </Typography>
+                            </CardContent>
 
-                            </Timeline>
-                        </Item>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <Card sx={{ maxWidth: 345, minWidth: 300 }}>
+                            <CardHeader
 
-                        <Item>
-                            <Typography variant='h3'>Projects</Typography>
-                        </Item>
+
+                                title="Space Pew Pew"
+                                subheader="Summer 2021"
+                            />
+                            <CardMedia
+                                component="img"
+                                height="194"
+                                image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                                alt="Paella dish"
+                            />
+                            <CardContent>
+                                <Typography variant="body2" color="text.secondary">
+                                    Shoot my friend bruce in a fun arcade-style 2D top-down shooting game!
+                                </Typography>
+                            </CardContent>
+
+                        </Card>
                     </Grid>
                 </Grid>
 
-            </div>
-
-
+            </Container>
         </div>
     );
 };
