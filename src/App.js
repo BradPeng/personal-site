7574287486805465fe53b51d5e85b96a5b23a980
Brad Peng <bradpeng778@gmail.com>
2022-02-13 18:35:46 -0800

    added bruce directory for three fibre project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Spacepewpew from './components/spacepewpew/spacepewpew';
 import Home from './components/home/home'
 import Rengoku from './components/rengoku/rengoku';
+import Threejs from './components/threejs/threejs';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/" exact element={<Home />} />
       <Route path="/spacepewpew" exact element={<Spacepewpew />} />
       <Route path="/rengoku" exact element={<Rengoku />} />
+      <Route path="/bruce" exact element={<Threejs />} />
       {/* <Route path="/" exact element={<Home />} />
       <Route path="/spacepewpew" exact element={<Spacepewpew />} /> */}
       </Routes>
