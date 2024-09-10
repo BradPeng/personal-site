a7a876b5ab9e2fc34cd4c73984898ed0629aaa78
Brad Peng <bradpeng778@gmail.com>
2024-09-10 01:20:27 -0700

    Add swarmed

---
+++ src/components/swarmed/swarmed.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Swarmed = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <iframe
+        src="/swarmed/index.html"
+        width="1920"
+        height="1080"
+        style={{ border: "none" }}
+        title="Game"
+      />
+    </div>
+  );
+};
+  
+export default Swarmed;
