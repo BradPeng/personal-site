49f9de8a164024e22d19687ea60873780e103dd5
Brad Peng <bradpeng778@gmail.com>
2024-09-11 16:16:01 -0700

    Add about me section

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,79 +1,148 @@
-import { Container, Typography } from '@mui/material';
-import Avatar from '@mui/material/Avatar';
-import Grid from '@mui/material/Grid';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Container, Typography } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "aos/dist/aos.css";
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import DownArrow from '../../images/downarrow.png';
-import GitHubLogo from '../../images/githublogo.png';
-import LinkedInLogo from '../../images/linkedinlogo.png';
-import ProjectCards from './projectcards';
-import './projectCarousel.css';
-import WorkHistory from './workhistory';
+import DownArrow from "../../images/downarrow.png";
+import GitHubLogo from "../../images/githublogo.png";
+import LinkedInLogo from "../../images/linkedinlogo.png";
+import ProjectCards from "./projectcards";
+import "./projectCarousel.css";
+import WorkHistory from "./workhistory";
 
-const handleScroll = ref => {
-    window.scrollTo({
-        behavior: "smooth",
-        top: ref.current.offsetTop
-    });
+const handleScroll = (ref) => {
+  window.scrollTo({
+    behavior: "smooth",
+    top: ref.current.offsetTop,
+  });
 };
 
 const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-    },
+  palette: {
+    mode: "dark",
+  },
 });
 
 function Home() {
-    const workSectionRef = useRef();
-    return (
-        <ThemeProvider theme={darkTheme}>
-            <div className="App">
-                <Container maxWidth='xl'>
-                    <div style={{ height: "100vh", flexDirection: "column", display: "flex", justifyContent: "center" }}>
-                        <Typography variant='h3' component='h1'>Brad Peng</Typography>
+  const workSectionRef = useRef();
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <Container maxWidth="xl">
+          <div
+            style={{
+              height: "100vh",
+              flexDirection: "column",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Typography variant="h3" component="h1">
+              Brad Peng
+            </Typography>
 
-                        <Typography gutterBottom variant='h5'>Computer Science, The University of British Columbia (2019 - 2024)</Typography>
-                        <Grid justifyContent="center" container spacing={1}>
-                            <Grid item>
-                                <Link target="_blank" rel="noopener" href="https://github.com/BradPeng">
-                                    <Avatar src={GitHubLogo} />
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link target="_blank" rel="noopener" href="https://www.linkedin.com/in/brad-peng-165206214/">
-                                    <Avatar style={{ backgroundColor: 'white' }} src={LinkedInLogo} />
-                                </Link>
-                            </Grid>
-                        </Grid>
+            <Typography gutterBottom variant="h5">
+              Computer Science, The University of British Columbia (2019 - 2024)
+            </Typography>
+            <Container style={{ paddingBottom: "16px" }}>
+              <Typography variant="body1" align="left">
+                Hi, I'm Brad, a Computer Science graduate from the University of
+                British Columbia. This website outlines my software development
+                journey, which has had a focus on Web and Game Development.
+                Below, you'll find some information about my past work
+                experiences, as well as some of the project's I made while
+                learning about different tech stacks. Some of my projects even
+                have convient web demo's available, so make sure to check them
+                out if you get a chance!
+              </Typography>
+            </Container>
+            <Grid justifyContent="center" container spacing={1}>
+              <Grid item>
+                <Link
+                  target="_blank"
+                  rel="noopener"
+                  href="https://github.com/BradPeng"
+                >
+                  <Avatar src={GitHubLogo} />
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link
+                  target="_blank"
+                  rel="noopener"
+                  href="https://www.linkedin.com/in/brad-peng-165206214/"
+                >
+                  <Avatar
+                    style={{ backgroundColor: "white" }}
+                    src={LinkedInLogo}
+                  />
+                </Link>
+              </Grid>
+            </Grid>
 
-                        <div style={{ width: 50, textAlign: 'center', position: 'absolute', bottom: 50, right: 0, left: 0, marginLeft: 'auto', marginRight: 'auto', cursor: "pointer" }}>
-                            <Link onClick={() => { handleScroll(workSectionRef) }}>
-                                <Avatar src={DownArrow} />
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div ref={workSectionRef} style={{ paddingTop: 75 }} id="work" />
-
-                    <WorkHistory />
-
-                    <ProjectCards />
-
-                </Container>
-
-                <Container style={{ paddingBottom: '250px' }}>
-                    <Typography variant='h3' component='h1'>Volunteer Work</Typography>
-                    <Link  target="_blank" rel="noopener" href='https://gameofapps.org/' gutterBottom variant='h4'>Game of Apps (2020 - Present)</Link>
-                    <Typography style={{paddingTop: 40}} align='left' variant='body1'>The Game of Apps program is a course available to public school students in the Greater Vancouver Area. The program's mission statement is to introduce real-world technology design and development to students and create interest in STEM fields. I have volunteered in both the senior and junior programs. The senior program teaches app design and development through Java and Android studio. I scripted and recorded lessons for the students and directly mentored one of the teams during the program. The junior program is a lot lighter and only teaches programming fundamentals. I spend my time during and after lectures answering student questions about basic coding principles and the Swift language.
-
-</Typography>
-                </Container>
+            <div
+              style={{
+                width: 50,
+                textAlign: "center",
+                position: "absolute",
+                bottom: 50,
+                right: 0,
+                left: 0,
+                marginLeft: "auto",
+                marginRight: "auto",
+                cursor: "pointer",
+              }}
+            >
+              <Link
+                onClick={() => {
+                  handleScroll(workSectionRef);
+                }}
+              >
+                <Avatar src={DownArrow} />
+              </Link>
             </div>
-        </ThemeProvider>
-    );
-};
+          </div>
+
+          <div ref={workSectionRef} style={{ paddingTop: 75 }} id="work" />
+        
+          <WorkHistory />
+
+          <ProjectCards />
+        </Container>
+
+        <Container style={{ paddingBottom: "250px" }}>
+          <Typography variant="h3" component="h1">
+            Volunteer Work
+          </Typography>
+          <Link
+            target="_blank"
+            rel="noopener"
+            href="https://gameofapps.org/"
+            gutterBottom
+            variant="h4"
+          >
+            Game of Apps (2020 - Present)
+          </Link>
+          <Typography style={{ paddingTop: 40 }} align="left" variant="body1">
+            The Game of Apps program is a course available to public school
+            students in the Greater Vancouver Area. The program's mission
+            statement is to introduce real-world technology design and
+            development to students and create interest in STEM fields. I have
+            volunteered in both the senior and junior programs. The senior
+            program teaches app design and development through Java and Android
+            studio. I scripted and recorded lessons for the students and
+            directly mentored one of the teams during the program. The junior
+            program is a lot lighter and only teaches programming fundamentals.
+            I spend my time during and after lectures answering student
+            questions about basic coding principles and the Swift language.
+          </Typography>
+        </Container>
+      </div>
+    </ThemeProvider>
+  );
+}
 
 export default Home;
