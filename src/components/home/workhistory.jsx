cb8e0f8686b6d35b2d327c6805deaf6c796e343e
Brad Peng <bradpeng778@gmail.com>
2024-09-11 17:02:20 -0700

    Add more text to workhistory

--- src/components/home/workhistory.jsx
+++ src/components/home/workhistory.jsx
@@ -42,25 +42,51 @@ const workHistoryList = [
   {
     title: "Software Development Intern",
     subtitle: "Alida",
-    description: "Yaba gaba goo",
+    description:
+      "During my time at Alida, I primarily worked on the team's Foundation Effort, which focused on breaking down our monolithic application architecture into a microservices-driven approach. This internship was where I first gained hands-on experience with REST and backend web infrastructure.",
+    bullets: [
+      "Rerouted data processing from a monolithic ASP.NET C# application to microservices by implementing RESTful APIs, reducing load and improving scalability.",
+      "Ensured stateless authentication on microservices with JWT validation, providing secure API access.",
+      "Developed integration tests to ensure data synchronization between the monolith's MSSQL database and the microservices' PostgreSQL databases to ensure smooth deployments.",
+      "Enhanced debugging with error and information logging to Kibana, improving system visibility and problem resolution.",
+    ],
   },
   {
     title: "Web Development Intern",
     subtitle: "Bank of Canada",
     description:
-      "As part of the front-end web team at the Bank of Canada, I was responsible for tasks regarding the six internal and external-facing sites used by the Bank of Canada. As an intern, I was allowed to work on any jobs that came up in our sprint boards, including implementation tasks and quality assurance testing. I was well known in the team to be an excellent peer reviewer, as my review process had uncovered many vulnerabilities and bugs. This saved a lot of time for the senior developers who reviewed pull requests. I also wrote many automated test scripts that ensured everything would behave as expected. In addition to minor bug fixes, I got the opportunity to implement various quality of life changes to the sites, such as adding a search feature to our drop-down menus or adding additional attributes to our components for increased customizability.",
+      "As a part of the front-end web team at the Bank of Canada, I contributed to the development and maintenance of six internal and external-facing sites. This internship provided valuable learning opportunities, allowing me to collaborate with experienced developers and solve challenges alongside fellow interns.",
+    bullets: [
+      "Assisted in migrating from amCharts 3 to amCharts 4 by updating WordPress components, ensuring seamless functionality and introducing new features.",
+      "Developed and executed BDD test cases with Behat, ensuring the functionality of web components.",
+      "Helped enable economists to visualize data on a web app using amCharts 4, facilitating research and data analysis.",
+      "Improved UX and resolved bugs, enhancing site functionality.",
+      "Achieved significant time savings in code review by identifying bugs through thorough quality assurance procedures.",
+    ],
   },
   {
     title: "Web Development Intern",
     subtitle: "Institute of Applied Design and Technology Education Society",
     description:
-      "This was my second time interning for this company. Like my previous internship with them, I was on a team that started development from scratch. Being a brand new project, we first started creating the base React components from which the site was constructed. The components were written in TypeScript and styled using the Styled Components library. Each component was also documented using React Storybook. Other developers could easily see each component in action and how its props affected its appearance and behaviour. After the base components and a few compound components were created, we assembled the site according to the design mocked up by our design team.",
+      "Similar to my previous internship, I joined a team tasked with building a project from the ground up—this time, a React-based website. It was during this internship that I first gained experience with both React and TypeScript, laying the foundation for my front-end development skills.",
+    bullets: [
+      "Developed and documented 10+ core React components, enhancing the website's flexibility with user-adjustable props for diverse use cases.",
+      "Assembled webpages using customizable React components and styled them with the Styled Components library.",
+      "Improved code coverage for React components, ensuring functionality was preserved during future updates.",
+      "Collaborated with the design team to ensure accurate implementation of design mockups.",
+    ],
   },
   {
     title: "iOS Development Intern",
     subtitle: "Institute of Applied Design and Technology Education Society",
     description:
-      "My first internship was with the Institute of Applied Design and Technology Education Society, a new non-profit hosting a mobile app development course and competition. I was on the team tasked with developing and launching the first version of the companion app for students to use while taking their course. I created a few screens, which involved creating views through Xcode's Storyboard functionality and wrote the associated code. I also did bug testing on various supported iOS devices throughout the development process. As we neared completion of the app, I also spent some time writing scripts that would populate and manage our Google Firebase backend with test data.",
+      "My first internship was with the Institute of Applied Design and Technology Education Society, a new non-profit hosting a mobile app development course and competition. I was part of the team responsible for developing and launching the first iOS version of the companion app for students. I contributed to the development of key app views and wrote scripts to manage our Google Firebase backend.",
+    bullets: [
+      "Created a parallax scrolling method that was applied to most views.",
+      "Brought design mockups to life using XVode's Storyboard functionality",
+      "Ensured layouts functioned on all current iOS devices through software and hardware testing",
+      "Wrote scripts that populated and managed our Firestore DB with mock data for development purposes.",
+    ],
   },
 ];
 
@@ -71,9 +97,23 @@ function WorkHistory() {
     <Card sx={{ maxWidth: 800 }} className={classes.timelineCard}>
       <CardContent key={workHistoryList.title}>
         <Container>
-          <Typography variant="h5">{workHistoryList.title}</Typography>
+          <Typography sx={{ fontWeight: "bold" }} variant="h5">
+            {workHistoryList.title}
+          </Typography>
           <Typography variant="h6">{workHistoryList.subtitle}</Typography>
-          <Typography variant="body1">{workHistoryList.description}</Typography>
+          <Typography gutterBottom variant="body1">
+            {workHistoryList.description}
+          </Typography>
+          {workHistoryList.bullets && (
+            <>
+              <Typography sx={{ fontWeight: "bold" }}>Work Summary</Typography>
+              <ul>
+                {workHistoryList.bullets.map((bullet, index) => (
+                  <li key={index}>{bullet}</li>
+                ))}
+              </ul>
+            </>
+          )}
         </Container>
       </CardContent>
     </Card>
